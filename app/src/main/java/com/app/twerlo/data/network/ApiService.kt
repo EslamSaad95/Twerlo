9015837c0fdd81f79cd02ca04112494e4e3feb60
Eslam <eslamsaad45@gmail.com>
2024-02-13 14:11:21 +0200

    add login api - login dto

--- app/src/main/java/com/app/twerlo/data/network/ApiService.kt
+++ app/src/main/java/com/app/twerlo/data/network/ApiService.kt
@@ -1,7 +1,18 @@
 package com.app.twerlo.data.network
 
+import com.app.twerlo.data.network.dto.LoginDto
+import retrofit2.Response
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+
 interface ApiService {
 
-
+  @POST("auth/login")
+  @FormUrlEncoded
+  suspend fun login(@Field("username")userName:String,
+                    @Field("password")password:String): Response<LoginDto>
 
 }
