c02d9cdbd4323c3868ead704abdc6849e7840e40
Eslam <eslamsaad45@gmail.com>
2024-02-13 21:12:41 +0200

    products repo injection

--- app/src/main/java/com/app/twerlo/data/di/ApiModule.kt
+++ app/src/main/java/com/app/twerlo/data/di/ApiModule.kt
@@ -1,7 +1,9 @@
 package com.app.twerlo.data.di
 
 import com.app.twerlo.data.repo.LoginRepositoryImpl
+import com.app.twerlo.data.repo.ProductsRepositoryImpl
 import com.app.twerlo.domain.repo.LoginRepo
+import com.app.twerlo.domain.repo.ProductsRepo
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
@@ -15,4 +17,8 @@ abstract class ApiModule {
   @Singleton
   @Binds
   abstract fun provideLoginRepo(repo: LoginRepositoryImpl): LoginRepo
+
+  @Singleton
+  @Binds
+  abstract fun provideProductsRepo(repo: ProductsRepositoryImpl): ProductsRepo
 }
